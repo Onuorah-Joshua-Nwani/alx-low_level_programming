13fc3b2d75860613626bbce0c8eb3e41b4fd2ef3
Onuorah-Joshua-Nwani <onuorahani@gmail.com>
2022-10-19 21:41:44 -0700

    Task 0, Test for Multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - tests functions that prints if integer is positive or negetive
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negetive(i);
+
+	return (0);
+}
