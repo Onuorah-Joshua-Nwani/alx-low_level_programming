01a1e3867eefcb39854b8361dcd77fc484585b69
Onuorah-Joshua-Nwani <onuorahani@gmail.com>
2022-10-22 00:16:26 -0700

    Task 12 101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0
+ *
+ */
+int main(void)
+{
+	int number;
+
+	printf("Enter an integer: ");
+	scanf("%d", &number);
+
+	printf("You entered: %d", number);
+
+	return (0);
+}
