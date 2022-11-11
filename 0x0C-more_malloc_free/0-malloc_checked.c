b9539669931bdb1e542225b527ade7f4bf4dee0e
Onuorah-Joshua-Nwani <onuorahani@gmail.com>
2022-11-11 10:12:58 -0800

    Task 0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdlib.h>
+
 
 /**
  * malloc_checked - array for prints a string
@@ -6,15 +8,14 @@
  * Return: void
  */
 
+
 void *malloc_checked(unsigned int b)
 {
-	void *p;
+	void *mem = malloc(b);
 
-	p = malloc(b);
-	if (p == NULL)
-	{
+	if (mem == NULL)
 		exit(98);
-	}
 
-	return (p);
+	return (mem);
 }
+
