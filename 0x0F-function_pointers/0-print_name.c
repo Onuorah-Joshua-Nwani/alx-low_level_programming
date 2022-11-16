f01a770226661b63a65488e8b3e6912eeccc9892
Onuorah-Joshua-Nwani <onuorahani@gmail.com>
2022-11-16 02:31:49 -0800

    Task 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -14,26 +14,3 @@ void print_name(char *name, void (*f)(char *))
 		return;
 	f(name);
 }
-
-1 - array_iterator.c
-
-#include "function_pointers.h"
-
-/**
- * array_iterator - executes a function given as a
- * parameter on each element of an array.
- * @array: input integer array.
- * @size: size of the array.
- * @action: pointer to the function.
- * Return: no return.
- */
-
-void array_iterator(int *array, size_t size, void (*action)(int))
-{
-	unsigned int i;
-
-	if (array && action)
-		for (i = 0; i < size; i++)
-			action(array[i]);
-}
-
