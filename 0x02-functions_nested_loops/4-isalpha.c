ccb7bd069b1604dea33f3afa6997e2dceac0af6d
Onuorah-Joshua-Nwani <onuorahani@gmail.com>
2022-12-05 01:30:53 -0800

    Task 1-create_file.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * _isalpha - checks for alphabets
- * @c: character to be checked
+ * _isalpha - checks for char alphabet
+ * @c: char to be checked
  *
- * Retun: 1 if character is a letter, 0 otherwise
+ * Retun: 1 if char is a lower or upper letter, 0 otherwise
  */
 int _isalpha(int c)
-			{
+{
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
 	else
